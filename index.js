07f500718667b00cfe5e61786f10387d6f3d8498
Merajul Islam <meraj.exe@gmail.com>
2023-06-11 02:41:57 +0600

    cartcomplete

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
       const result = await coursesCollection.find().toArray();
       res.send(result);
     });
+    // carts
+
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
 
     app.post('/carts', async (req, res) => {
       const item = req.body;
