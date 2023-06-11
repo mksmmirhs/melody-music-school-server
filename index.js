75626b7b20c2db163a376bf8c7e2c6286d11ea9f
Merajul Islam <meraj.exe@gmail.com>
2023-06-11 23:21:15 +0600

    instructor check api

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' };
+      res.send(result);
+    });
+
+    app.get('/users/instructor/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === 'instructor' };
+      res.send(result);
+    });
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
